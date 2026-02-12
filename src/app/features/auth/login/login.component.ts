d7905315c12d63a3f6d889a483c1388542abe060
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-12 23:01:58 +0000

    refactor login component to improve form handling and add success message support

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,37 +1,73 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { RouterLink } from '@angular/router';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import * as AuthActions from '../../../core/store/auth/auth.actions';
-import { selectAuthLoading, selectAuthError } from '../../../core/store/auth/auth.selectors';
+import { selectAuthLoading, selectAuthError, selectSuccessMessage } from '../../../core/store/auth/auth.selectors';
+import { AuthBrandingComponent } from '../components/auth-branding/auth-branding.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, AuthBrandingComponent],
   templateUrl: './login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
   loading$;
   error$;
+  successMessage$;
+  showPassword = false;
+  returnUrl: string = '/jobs';
 
+  private destroy$ = new Subject<void>();
   private fb = inject(FormBuilder);
   private store = inject(Store);
+  private route = inject(ActivatedRoute);
 
   constructor() {
     this.loading$ = this.store.select(selectAuthLoading);
     this.error$ = this.store.select(selectAuthError);
+    this.successMessage$ = this.store.select(selectSuccessMessage);
+
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', [Validators.required]],
+      rememberMe: [false]
     });
   }
 
-  onSubmit() {
+  ngOnInit(): void {
+    this.route.queryParams.pipe(takeUntil(this.destroy$)).subscribe(params => {
+      this.returnUrl = params['returnUrl'] || '/jobs';
+    });
+
+    this.store.dispatch(AuthActions.clearError());
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
     if (this.loginForm.valid) {
-      this.store.dispatch(AuthActions.login(this.loginForm.value));
+      const { email, password } = this.loginForm.value;
+      this.store.dispatch(AuthActions.login({ email, password }));
+    } else {
+      Object.keys(this.loginForm.controls).forEach(key => {
+        this.loginForm.get(key)?.markAsTouched();
+      });
     }
   }
+
+  get emailControl() { return this.loginForm.get('email'); }
+  get passwordControl() { return this.loginForm.get('password'); }
 }
