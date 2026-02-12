7ea831763885dea3392bc8857b344b4fd49d7169
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-12 23:00:21 +0000

    update environment configuration to include Adzuna API credentials and clear apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: '',
+    adzunaAppId: '8d4c6933',
+    adzunaAppKey: 'f4075697b41601393e8e1943cf2fa587'
 };
