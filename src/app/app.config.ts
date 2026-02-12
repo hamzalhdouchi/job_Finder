6c5f4175d89c84d60cda165a526028e6d91a4544
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-12 22:58:14 +0000

    add favorites feature with reducer and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { authReducer } from './core/store/auth/auth.reducer';
 import { AuthEffects } from './core/store/auth/auth.effects';
+import { favoritesReducer } from './features/favorites/store/favorites.reducer';
+import { FavoritesEffects } from './features/favorites/store/favorites.effects';
 
 import { routes } from './app.routes';
 
@@ -14,9 +16,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
-    provideStore({ auth: authReducer }),
-    provideEffects([AuthEffects]),
+    provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
+    provideStore({ auth: authReducer, favorites: favoritesReducer }),
+    provideEffects([AuthEffects, FavoritesEffects]),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
 };
