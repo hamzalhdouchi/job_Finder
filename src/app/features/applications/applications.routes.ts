d327050978fc415525d8c8a454c0f777ce376340
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-12 22:59:02 +0000

    add applications list route to applications.routes.ts

--- src/app/features/applications/applications.routes.ts
+++ src/app/features/applications/applications.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ApplicationsListComponent } from './applications-list.component';
 
-export const applicationsRoutes: Routes = [];
+export const applicationsRoutes: Routes = [
+  {
+    path: '',
+    component: ApplicationsListComponent
+  }
+];
